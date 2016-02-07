88bd89d55a011aefd3535c59559154c49f773631
Sayutin Dmitry <cdkrot@yandex.ru>
2016-02-07 16:39:54 +0300

    Add human friendly names to syscalls detector

--- src/detect_syscalls.c
+++ src/detect_syscalls.c
@@ -7,6 +7,65 @@
 
 #include <stdio.h>
 
+#ifdef __x86_64
+  typedef long long int register_type;
+  #define REG_FORMAT "%lld"
+  #define ORIG_EAX orig_rax
+  #define EAX      rax
+  #define EBX      rbx
+  #define ECX      rcx
+  #define EDX      rdx
+#else
+  typedef long int      register_type;
+  #define REG_FORMAT "%ld"
+  #define ORIG_EAX orig_eax
+  #define EAX      eax
+  #define EBX      ebx
+  #define ECX      ecx
+  #define EDX      edx
+#endif
+
+const char* get_syscall_name(register_type id) {
+    switch (id) {
+        case SYS_write:
+            return "write";
+        case SYS_read:
+            return "read";
+        case SYS_open:
+            return "open";
+        case SYS_close:
+            return "close";
+        case SYS_execve:
+            return "execve";
+        case SYS_exit:
+            return "exit";
+        case SYS_mmap:
+            return "mmap";
+        case SYS_munmap:
+            return "munmap";
+        case SYS_mprotect:
+            return "mprotect";
+        case SYS_lseek:
+            return "lseek";
+        case SYS_access:
+            return "access";
+        case SYS_lstat:
+            return "lstat";
+        case SYS_brk:
+            return "brk";
+        case SYS_fstat:
+            return "fstat";
+        case SYS_exit_group:
+            return "exit_group";
+        default:
+            return "unknown";
+    }
+}
+
+void get_syscall_descr(char* buf, size_t sz, register_type id) {
+    snprintf(buf, sz, "%s (" REG_FORMAT ")", get_syscall_name(id), id);
+}
+    
 int main(int argc, char** argv) {
     if (argc < 2)
         return -1;
@@ -22,6 +81,7 @@ int main(int argc, char** argv) {
     } else {
         int status;
         int insyscall = 0;
+        char buf[100];
         while(1) {
             wait(&status);
             if(WIFEXITED(status))
@@ -29,20 +89,16 @@ int main(int argc, char** argv) {
 
             struct user_regs_struct registers;
             ptrace(PTRACE_GETREGS, child, 0, &registers);
+
+            get_syscall_descr(buf, sizeof(buf), registers.ORIG_EAX);
             
-            if (insyscall == 0) {// syscall start
-                #ifndef __x86_64__
-                printf("Sys call %ld, params %ld %ld %ld\n", registers.orig_eax, registers.ebx, registers.ecx, registers.edx);
-                #else
-                printf("Sys call %lld, params %lld %lld %lld\n", registers.orig_rax, registers.rbx, registers.rcx, registers.rdx);
-                #endif
+            if (insyscall == 0) {
+                // syscall start                 
+                printf("Sys call %s, params " REG_FORMAT " " REG_FORMAT " " REG_FORMAT "\n",
+                       buf, registers.EBX, registers.ECX, registers.EDX);
             } else {
                 //syscall return
-                #ifndef __x86_64__
-                printf("Sys call %ld, return %ld\n", registers.orig_eax, registers.eax);
-                #else
-                printf("Sys call %lld, return %lld\n", registers.orig_rax, registers.rax);
-                #endif
+                printf("Sys call %s, return " REG_FORMAT "\n", buf, registers.EAX);
             }
             insyscall = 1 - insyscall;
             
